59d7bc29a9c1b1a6da0e2d77b0f39ba971a49355
David van Boxtel <davidvb00@gmail.com>
2023-12-20 13:40:25 +0100

    update

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -40,11 +40,96 @@
 <Navbar />
 
 <main>
+	<section class="primary-section">
+		<h1>1</h1>
+	</section>
+
+	<LayoutView {filteredWishes} {searchInput} {searchWishes} />
+
+	<section id="custom-view" class="grid-overview wishes">
+		<article>
+			{#each filteredWishes as wish}
+				<WishOverview {wish} {filteredWishes} />
+			{/each}
+		</article>
+	</section>
+
+	<section class="primary-section">
+		<h1>2</h1>
+	</section>
 	<div class="sub-header">
-		<div class="risoprint-container block_1">
+		<div class="risoprint-container block_1 hover-area">
 			<div class="layer base-layer"><h2>Overizchtspagina</h2></div>
 			<div class="layer color-layer1"></div>
 			<div class="layer color-layer2"></div>
+			<div class="hidden-object">
+				<svg
+					class="flower"
+					xmlns="http://www.w3.org/2000/svg"
+					viewBox="0 0 256 256"
+					id="IconChangeColor"
+					height="200"
+					width="200"
+					><rect width="256" height="256" fill="none"></rect><circle
+						cx="128"
+						cy="128"
+						r="28"
+						fill="none"
+						stroke="#000"
+						stroke-linecap="round"
+						stroke-linejoin="round"
+						stroke-width="8"
+					></circle><path
+						d="M115,103.2c-7.3-15.4-15-34.6-15-47.2a28,28,0,0,1,56,0c0,12.6-7.7,31.8-15,47.2"
+						fill="#ffffff"
+						stroke="#000"
+						stroke-linecap="round"
+						stroke-linejoin="round"
+						stroke-width="8"
+						id="mainIconPathAttribute"
+					></path><path
+						d="M100,126.8c-17-1.3-37.5-4.3-48.4-10.6a28,28,0,0,1,28-48.4C90.5,74,103.3,90.3,113,104.4"
+						fill="#ffffff"
+						stroke="#000"
+						stroke-linecap="round"
+						stroke-linejoin="round"
+						stroke-width="8"
+						id="mainIconPathAttribute"
+					></path><path
+						d="M113,151.6c-9.7,14.1-22.5,30.4-33.4,36.6a28,28,0,1,1-28-48.4c10.9-6.3,31.4-9.3,48.4-10.6"
+						fill="#ffffff"
+						stroke="#000"
+						stroke-linecap="round"
+						stroke-linejoin="round"
+						stroke-width="8"
+						id="mainIconPathAttribute"
+					></path><path
+						d="M141,152.8c7.3,15.4,15,34.6,15,47.2a28,28,0,0,1-56,0c0-12.6,7.7-31.8,15-47.2"
+						fill="#ffffff"
+						stroke="#000"
+						stroke-linecap="round"
+						stroke-linejoin="round"
+						stroke-width="8"
+						id="mainIconPathAttribute"
+					></path><path
+						d="M156,129.2c17,1.3,37.5,4.3,48.4,10.6a28,28,0,0,1-28,48.4c-10.9-6.2-23.7-22.5-33.4-36.6"
+						fill="#ffffff"
+						stroke="#000"
+						stroke-linecap="round"
+						stroke-linejoin="round"
+						stroke-width="8"
+						id="mainIconPathAttribute"
+					></path><path
+						d="M143,104.4c9.7-14.1,22.5-30.4,33.4-36.6a28,28,0,0,1,28,48.4c-10.9,6.3-31.4,9.3-48.4,10.6"
+						fill="#ffffff"
+						stroke="#000"
+						stroke-linecap="round"
+						stroke-linejoin="round"
+						stroke-width="8"
+						id="mainIconPathAttribute"
+					></path></svg
+				>
+			</div>
 		</div>
 		<div class="risoprint-container block_2">
 			<div class="layer base-layer"><h2>over</h2></div>
@@ -67,27 +152,200 @@
 			{/each}
 		</article>
 	</section>
+
+	<section class="primary-section">
+		<h1>3</h1>
+	</section>
+
+	<section class="thunder">
+		<div id="lightning-container">
+			<h1 id="breathing-header">Alle Wensen</h1>
+			<div class="rain"></div>
+		</div>
+	</section>
+
+	<LayoutView {filteredWishes} {searchInput} {searchWishes} />
+
+	<section id="custom-view" class="grid-overview wishes">
+		<article>
+			{#each filteredWishes as wish}
+				<WishOverview {wish} {filteredWishes} />
+			{/each}
+		</article>
+	</section>
 </main>
 
 <style>
-	/* Magical risoprint animation */
+	/* Algemeen */
+
+	.primary-section {
+		margin-top: 100px;
+		margin-bottom: 20px;
+	}
+
+	/* Magical risoprint animation 3 */
+	.thunder {
+		height: 300px;
+		width: 100%;
+		margin-top: 50px;
+		margin-bottom: 50px;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		background-color: black;
+		color: white;
+		animation: thunder 0.3s linear infinite;
+		
+	}
+
+	@keyframes thunder {
+		0% {
+			background-position: 0% 0%;
+		}
+		100% {
+			background-position: 20% 100%;
+		}
+	}
+
+	#breathing-header {
+		font-size: 3rem;
+		text-align: center;
+		color: #00bfff; /* Deep Sky Blue */
+		animation:
+			glow 1.5s ease-in-out infinite alternate,
+			breathe 3s ease-in-out infinite;
+	}
+
+	@keyframes glow {
+		from {
+			text-shadow:
+				0 0 10px #00bfff,
+				0 0 20px #00008b,
+				0 0 30px #00008b,
+				0 0 40px white; /* Sky Blue, Dark Blue, White */
+		}
+		to {
+			text-shadow:
+				0 0 5px white,
+				0 0 15px #00008b,
+				0 0 25px #00bfff,
+				0 0 35px #00008b; /* White, Dark Blue, Sky Blue */
+		}
+	}
+
+	@keyframes breathe {
+		0%,
+		100% {
+			transform: scale(1);
+		}
+		50% {
+			transform: scale(1.1);
+		}
+	}
+
+	.rain::before {
+		content: '';
+		position: absolute;
+		width: 100%;
+		height: 100%;
+		background-image: url('https://w7.pngwing.com/pngs/428/585/png-transparent-white-lightning-illustration-lightning-strike-lightning-blue-computer-wallpaper-desktop-wallpaper.png');
+		/* top: -100px; */
+		transform: rotate(180deg);
+		animation: lightining 4s linear infinite;
+		opacity: 1;
+	}
+	@keyframes lightining {
+		0% {
+			opacity: 0;
+		}
+		20% {
+			opacity: 0;
+		}
+		21% {
+			opacity: 1;
+		}
+		25% {
+			opacity: 0;
+		}
+		30% {
+			opacity: 0;
+		}
+		31% {
+			opacity: 1;
+		}
+		35% {
+			opacity: 0;
+		}
+		100% {
+			opacity: 0;
+		}
+	}
+
+	/* Magical risoprint animation 2 */
+
+	.flower {
+		position: absolute;
+		left: 0;
+		top: 5px;
+		height: 50px;
+	}
+
+	.hover-area {
+		/* Your styles for the hover area */
+		width: 200px;
+		height: 200px;
+		border: 1px solid black;
+		position: relative;
+	}
+
+	.hidden-object {
+		/* Initially hide the object */
+		display: none;
+		position: absolute;
+		top: 0;
+		left: 0;
+		opacity: 50;
+
+		/* Styles for the object */
+		background-color: lightblue;
+		width: 100%;
+		height: 100%;
+		text-align: center;
+		line-height: 200px; /* Example to center text vertically */
+	}
+
+	.hover-area:hover .hidden-object {
+		/* Display the object when hover-area is hovered */
+		display: block;
+		animation: fadeIn 1s; /* Example animation */
+	}
+
+	@keyframes fadeIn {
+		from {
+			opacity: 0;
+		}
+		to {
+			opacity: 5;
+		}
+	}
+
 	.block_1 {
 		position: relative;
 		width: 100%;
-		height: 100px;
+		height: 65px;
 	}
 	.block_2 {
 		position: relative;
 		grid-column: auto/span 2;
 		width: 100%;
-		height: 100px;
+		height: 65px;
 	}
 	.block_3 {
 		position: relative;
 		grid-column: auto/span 3;
 		grid-row: auto/span 2;
 		width: 100%;
-		height: 200px;
+		height: 100px;
 	}
 	.sub-header {
 		display: grid;
@@ -128,10 +386,6 @@
 	.color-layer2 {
 		/* ... */
 		z-index: 4;
-	}
-
-	.base-layer {
-		background-color: #f0e4d7;
 	}
 
 	.color-layer1 {
